93bbc3bde31dad0d7721921848e9f8b44b728af1
Bhavika Patil <bypatil1602@gmail.com>
2024-10-13 01:32:10 +0530

    Shared Preferences

---
+++ app/src/main/java/com/example/sharedpreference/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.sharedpreference
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextUsername: EditText
+    private lateinit var buttonSave: Button
+    private lateinit var buttonRetrieve: Button
+    private lateinit var textViewResult: TextView
+
+    // Key for SharedPreferences
+    private val PREF_NAME = "MyPrefs"
+    private val Key_name = "Username"
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        //initialize the value
+        editTextUsername = findViewById(R.id.editTextUsername)
+        buttonSave = findViewById(R.id.buttonSave)
+        buttonRetrieve = findViewById(R.id.buttonRetrieve)
+        textViewResult = findViewById(R.id.textViewResult)
+
+        //save button click listner
+        buttonSave.setOnClickListener {
+            val name = editTextUsername.text.toString()
+            if (name.isNotEmpty()) {
+                saveNameToPreference(name)
+            }else{
+                Toast.makeText(this, "plz enter your name",Toast.LENGTH_SHORT).show()
+            }
+        }
+        //retrieve button click listener
+        buttonRetrieve.setOnClickListener {
+            val savedName = retrieveNameFromPreference()
+            if (savedName.isNotEmpty()){
+                textViewResult.text = "Retrived Name:$savedName"
+            }else{
+                textViewResult.text= "No name saved yet!"
+            }
+        }
+
+    }
+
+    private fun saveNameToPreference(name: String) {
+        val sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(Key_name, name)
+        editor.apply()
+        Toast.makeText(this,"Name saved successfully ",Toast.LENGTH_SHORT).show()
+    }
+
+    private fun retrieveNameFromPreference():String {
+        val sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        return sharedPreferences.getString(Key_name, "")?:""
+
+    }
+
+
+}
